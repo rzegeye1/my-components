8e53340ead6d52b2b429b77b881fc5f149ed69a0
Rodas Zegeye <rodaszegeye@Rodass-MacBook-Pro.local>
2022-08-31 12:18:36 -0400

    add button component

---
+++ src/Button.tsx
@@ -0,0 +1,27 @@
+import React, { HTMLAttributes, ReactNode } from 'react'
+
+export interface Props extends HTMLAttributes<HTMLButtonElement> {
+    /** Provide a text for the button */
+    children: ReactNode;
+     
+    /** which variant look would you like to use */
+    variant: 'primary' | 'secondary';
+    
+}
+
+/** This is a specail button */
+export const Button = ({children, variant = 'primary', ...props}: Props) => {
+    return (
+        <button {...props} style={{background: variant === 'primary'? 'blue': 'gray', 
+        color: 'white',
+        border: 'none',
+        borderRadius: '200',
+        padding: 10,
+        cursor: 'pointer'
+        }}>
+            {children}
+        </button>
+    )
+}
+
+
